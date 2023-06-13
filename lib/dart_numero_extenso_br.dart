9b4e3c70e3e284ae23895c5b948dd63deea76da2
Leonardo de Albuquerque Gouveia <leogouv@gmail.com>
2023-06-13 11:56:28 -0300

    Revert "Cria Classe para converter extenso em numeros"
    This reverts commit 0b58b8c17d241057545de8752c8af70046db84d7.

--- lib/dart_numero_extenso_br.dart
+++ lib/dart_numero_extenso_br.dart
@@ -3,7 +3,18 @@ library numero_extenso;
 import 'dart_extenso_numero.dart';
 import 'dart_numero_extenso.dart';
 
+<<<<<<< HEAD
 class Extenso {
+=======
+/// Um método para converter números para valor monetário por extenso
+class Extenso {
+  /// Retorna o número monetário por extenso, portugues do Brasil
+  /// Código convertido do PHP dos seguintes links:
+  /// https://pt.stackoverflow.com/questions/99460/como-converter-n%C3%BAmero-em-float-para-n%C3%BAmero-por-extenso-no-php
+  /// https://recursosdophp.blogspot.com/2012/04/ola-seguidores-do-php-bom-eu-fiz.html
+  /// Autor original desconhecido
+  ///
+>>>>>>> parent of 0b58b8c (Cria Classe para converter extenso em numeros)
   static String descrever(double valor) {
     return NumeroParaExtenso.descrever(valor);
   }
--- test/dart_numero_extenso_br_test.dart
+++ test/dart_numero_extenso_br_test.dart
@@ -2,6 +2,7 @@ import 'package:dart_numero_extenso_br/dart_numero_extenso_br.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+<<<<<<< HEAD
   test('Transcrever extenso para números', () {
     expect(
       Extenso.converter(
@@ -16,5 +17,14 @@ void main() {
       Extenso.descrever(14051234567890.63),
       'quatorze trilhões, cinquenta e um bilhões, duzentos e trinta e quatro milhões, quinhentos e sessenta e sete mil, oitocentos e noventa reais e sessenta e três centavos',
     );
+=======
+  test('Transcrever números para extenso', () {
+    expect(Extenso.descrever(2), 'dois reais');
+    expect(Extenso.descrever(1), 'um real');
+    expect(Extenso.descrever(0), 'zero');
+    expect(Extenso.descrever(0.05), 'cinco centavos');
+    expect(Extenso.descrever(14051234567890.63),
+        "quatorze trilhões, cinquenta e um bilhões, duzentos e trinta e quatro milhões, quinhentos e sessenta e sete mil, oitocentos e noventa reais e sessenta e três centavos");
+>>>>>>> parent of 0b58b8c (Cria Classe para converter extenso em numeros)
   });
 }
